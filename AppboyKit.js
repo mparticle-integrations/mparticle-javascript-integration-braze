37cf383200fb679dfc78fd0ac9895c4471083d87
briancaw <brian@appboy.com>
2017-05-09 14:35:59 -0400

    Adds initialization options for HTML IAMs and SDK Flavor

--- AppboyKit.js
+++ AppboyKit.js
@@ -223,6 +223,8 @@
                 isTesting = testMode;
                 // 30 min is Appboy default
                 options.sessionTimeoutInSeconds = forwarderSettings.ABKSessionTimeoutKey || 1800;
+                options.sdkFlavor = 'mparticle';
+                options.enableHtmlInAppMessages = forwarderSettings.enableHtmlInAppMessages == 'True'
 
                 if (testMode !== true) {
                     /* eslint-disable */
