34c9f263023703ff2b4135f5f8f2edd6ddda9169
iherdt-mparticle <54378555+iherdt-mparticle@users.noreply.github.com>
2022-05-03 14:16:40 -0700

    Adding customFlags.BRAZE parameter
    Adding the customFlags parameter that allows to expose an API to pass any optional initialization parameters to the Braze web SDK.

--- src/BrazeKit-dev.js
+++ src/BrazeKit-dev.js
@@ -39,7 +39,8 @@ var constructor = function() {
     var self = this,
         forwarderSettings,
         options = {},
-        reportingService;
+        reportingService,
+        mpCustomFlags;
 
     self.name = name;
 
@@ -358,8 +359,9 @@ var constructor = function() {
         }
     }
 
-    function initForwarder(settings, service, testMode) {
+    function initForwarder(settings, service, testMode, customFlags) {
         // eslint-disable-line no-unused-vars
+        mpCustomFlags = customFlags;
         try {
             forwarderSettings = settings;
             reportingService = service;
@@ -394,6 +396,13 @@ var constructor = function() {
                     options.baseUrl = customUrl;
                 }
             }
+            
+            if (mpCustomFlags && mpCustomFlags['BRAZE']) {
+                for (var key in mpCustomFlags.BRAZE) {
+                    options[key] = mpCustomFlags.BRAZE[key]
+                }
+            }
+            
             if (testMode !== true) {
                 appboy.initialize(forwarderSettings.apiKey, options);
                 finishAppboyInitialization(forwarderSettings);
