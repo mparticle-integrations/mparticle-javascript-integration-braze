8c3fdb10c9ab2584e90fdc985dde8e6832f82a81
Robert Ing <ring@mparticle.com>
2024-10-21 17:00:48 -0400

    feat: Implement Google EU Consent

---
+++ rollup.test.config.js
@@ -0,0 +1,21 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default [
+    {
+        input: 'test/tests.js',
+        output: {
+            file: 'test/test-bundle.js',
+            format: 'iife',
+            exports: 'named',
+            name: 'mpBrazeKit',
+            strict: false,
+        },
+        plugins: [
+            resolve({
+                browser: true,
+            }),
+            commonjs(),
+        ],
+    },
+];
